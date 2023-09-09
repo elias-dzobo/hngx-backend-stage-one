3cf869d5f0470f0aee0bab9c6343bcc1d543360e
elias-dzobo <elias.dzobo@gmail.com>
2023-09-09 20:08:55 +0000

    stage one task complete

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from datetime import datetime, timezone
+import json 
+
+week_days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+app = FastAPI()
+
+
+
+@app.get('/api')
+def stage_config(slack_name: str, track: str):
+    response = {
+        'slack_name': slack_name,
+        'current_day': week_days[int(datetime.now().date().strftime('%w'))],
+        'utc_time': datetime.utcnow().replace(tzinfo=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
+        'track': track,
+        'github_file_url': '',
+        'github_repo_url': '',
+        'status_code': 200
+    }
+
+    data = json.dumps(response)
+
+    return data 
